23c8177f276c14f14b61078da3eb1d49975fcd8b
Abzed <ibraabzed@gmail.com>
2022-08-17 14:48:52 +0300

    commit tree af7187959caca658c013f475aaa6ca5e56b85aa3

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
-class Student 
+require_relative "./user"
+require "pry"
+class Student < User
 
 end
+
+student = Student.new("Steve", "Jobs")
+student.first_name
+student.last_name
+
+# binding.pry
